0c321f6af7bdfa8c31417c5d88e65524ea10b655
Michael Condon <47602908+snakeycoder@users.noreply.github.com>
2019-02-14 11:41:35 +0000

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,26 +1,34 @@
+import math
 pList=[2,3,5,7]
 def checker(i):
     for j in pList:
-        if j>int(np.sqrt(i)+1):
+        if j>int(math.sqrt(i)+1):
             break
         if i%j==0:
             return(False)
     pList.append(i)
     return(True)
 
-for i in range(8,200):
+for i in range(pList[-1]+1,200):
     checker(i)
-initDict={}
 
-for j in range(2,200,2):
-    counter=0
+
+n,w=map(int,input().split())
+j=0
+counter2=0
+while counter2<n:
     k=0
-    while pList[k]<=(j//2):
-        #if pList[k]>=j-pList[k]:
-        #    print("oopsy!")
+    counter=0
+    ways=[]
+    while pList[k]<=(j//2+1):
+        ways.append(pList[k])
         k+=1
-        if j-pList[k] in pList:
+        if j-pList[k] in pList and j-pList[k] not in ways:
             counter+=1
-            print(j,pList[k],j-pList[k])
-    initDict[j]=counter
-print(initDict)
+        if counter>w+1:
+            break
+    if counter==w:
+        counter2+=1
+    if counter2==n:
+        print(j)
+    j+=1
